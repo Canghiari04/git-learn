d004c666c930fcc6f487b2fa94a4109fcb4e2875
Canghiari04 <matteo.canghiari@gmail.com>
2025-04-16 23:29:14 +0200

    .

--- project/src/App.js
+++ project/src/App.js
@@ -1,17 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from './pages/home.js';
+import HomePage from './pages/homePage.js';
 import LearnPage from './pages/learnPage.js';
 
+import { useState, createContext } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export const GitLearnContext = createContext();
+
 function App() {
+    const [language, setLanguage] = useState("IT")
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/learn" element={<LearnPage/>}/>
-            </Routes>
-        </BrowserRouter>
-    )
+        <GitLearnContext.Provider value={language}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage onChange={setLanguage}/>}/>
+                    <Route path="/learn" element={<LearnPage onChange={setLanguage}/>}/>
+                </Routes>
+            </BrowserRouter>
+        </GitLearnContext.Provider>
+    );
 }
 
 export default App;
