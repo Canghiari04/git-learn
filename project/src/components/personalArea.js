9d46d0d64ee38c08bf6d05efc2e1e9e06b95e6da
Canghiari04 <matteo.canghiari@gmail.com>
2025-05-31 17:10:04 +0200

    cleaned code

--- project/src/components/personalArea.js
+++ project/src/components/personalArea.js
@@ -22,7 +22,7 @@ export function PersonalArea({ setLogged }) {
                 <div className="div-second-column">{component}</div>
             </div>
         </>
-    )
+    );
 }
 
 function List({ setLogged, setComponent }) {
